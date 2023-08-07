cbdebdc5de1d9d11f9ac8e11ca76c836baa74062
Akash P Raj <akashpraj.bri@gmail.com>
2023-08-07 15:24:57 +0400

    forth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,23 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main>
+      <header data-elementor-type="header" data-elementor-id="1446" className="elementor elementor-1446 elementor-location-header">
+        <div className="elementor-section-wrap">
+          <section className="elementor-section elementor-top-section elementor-element elementor-element-e17c08c elementor-section-full_width elementor-section-content-middle elementor-section-height-min-height elementor-section-height-default elementor-section-items-middle elementor-sticky elementor-sticky--active elementor-section--handles-inside" data-id="e17c08c" data-element_type="section" data-settings="{&quot;sticky&quot;:&quot;top&quot;,&quot;background_background&quot;:&quot;classic&quot;,&quot;sticky_effects_offset&quot;:145,&quot;sticky_on&quot;:[&quot;desktop&quot;,&quot;tablet&quot;,&quot;mobile&quot;],&quot;sticky_offset&quot;:0}" style={{ position: 'fixed', width: '343px', marginTop: '0px', marginBottom: '0px', top: '0px' }}>
+            <div className="elementor-container elementor-column-gap-default">
+              <div className="elementor-row">
+                <div className="elementor-column elementor-col-16 elementor-top-column elementor-element elementor-element-4f1a89c animated fadeInDown" data-id="4f1a89c" data-element_type="column" data-settings="{&quot;animation&quot;:&quot;fadeInDown&quot;,&quot;animation_delay&quot;:300}">
+                  <div className="elementor-column-wrap elementor-element-populated">
+                    <div className="elementor-widget-wrap">
+                      <div className="elementor-element elementor-element-e941874 elementor-widget elementor-widget-heading" data-id="e941874" data-element_type="widget" data-widget_type="heading.default">
+                        <div className="elementor-widget-container">
+                          <h2 className="elementor-heading-title elementor-size-default"><a href="./">SHARUK &amp; MISBHA</a></h2>		</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div></section></div></header>
       <div className='elementor elementor-258'>
         <div className='elementor-inner'>
           <div className='elementor-section-wrap'>
